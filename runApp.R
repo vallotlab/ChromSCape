0f40520389713d8d15309b34311f8944e71c15a5
Pacome <pacome.prompsy@curie.fr>
2019-07-01 14:32:39 +0200

    Added gif
    Former-commit-id: 94c346dcc5f8a7b1e9d450f7207edc44f74200a4

--- runApp.R
+++ runApp.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 #runApp('/run/user/1001/gvfs/smb-share:server=zserver,share=umr3244/equipe_vallot/d_sebukhan/Shiny_App')
-runApp('/home/pprompsy/Documents/GitLab/Shiny_scChIPseq_stable_v1/Shiny_scChIP', launch.browser=TRUE)
+runApp('/home/pprompsy/Documents/GitLab/Shiny_scChIPseq_V1_github/', launch.browser=TRUE)
 
 #To redeploy app on server :
 #deployApp("/home/depic2/Documents/bioinfo/Shiny_App",appName='ChiP-SeqDataMining')
