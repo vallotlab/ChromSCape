9cde370388c47396b0ec239af08020c63ae4b811
Pacome <pacome.prompsy@curie.fr>
2019-07-23 11:40:19 +0200

    Fixed hc_cor tab + GSEA tsne gene list

--- runApp.R
+++ runApp.R
@@ -1,3 +1,3 @@
-library(shiny)
-
-runApp('/home/pprompsy/Documents/GitLab/Shiny_scChIPseq_V1_github/', launch.browser=TRUE)
+  library(shiny)
+  
+  runApp('/home/pprompsy/Documents/GitLab/Shiny_scChIPseq_V1_github/', launch.browser=TRUE)
