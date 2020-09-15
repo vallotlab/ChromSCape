f3301a001dfe9ea0f843a27132fce3dc4a5a6bc5
Pacome <pacome.prompsy@curie.fr>
2020-09-15 09:37:09 +0200

    Updated tests to R4.02
    Former-commit-id: ad380bf1e606608115178a33f522b1b0a17c6724
    Former-commit-id: 261e5198d78f4832f4f5fdae975f185bd8c5e075

--- tests/testthat/test_correlation_clustering.R
+++ tests/testthat/test_correlation_clustering.R
@@ -2,71 +2,6 @@ context("Testing correlation & clustering results.")
 
 library(testthat)
 
-# Functions for testing purposes
-create_scDataset_raw <- function(cells=300,features=600,
-                                 featureType = c("window","peak","gene"),
-                                sparse=TRUE, nsamp=4, ref = "hg38",batch_id = rep(1,nsamp)) {
-  
-  stopifnot(featureType %in% c("window","peak","gene"), ref %in% c("mm10","hg38"),
-            nsamp >= 1, cells >= nsamp, features >=1, length(batch_id) == nsamp)
-  
-  stopifnot()
-  
-  set.seed(47)
-  
-  # Create cell names
-  cell_counts = sapply(split( 1:cells , sample(nsamp, cells , repl = TRUE) ), length)
-  cell_names = sample = batches = list()
-  for(i in 1:length(cell_counts)) {
-    cell_names[[i]] = paste0("sample_",i,"_c", 1:cell_counts[i])
-    sample[[i]] = rep(paste0("sample_",i),cell_counts[i])
-    batches[[i]] = rep(batch_id[i],cell_counts[i])
-  }
-  cell_names = as.character(unlist(cell_names))
-  sample = as.character(unlist(sample))
-  batches = as.numeric(unlist(batches))
-  
-  # Create feature names
-  eval(parse(text = paste0("chr <- ChromSCape::",ref,".chromosomes"))) #load species chromsizes
-  chr = GRanges(chr)
-  
-  if(featureType[1] == "window") {
-    chr_ranges = unlist(tileGenome(setNames(width(chr),seqnames(chr)),
-                                      ntile = features))[1:features] # ~constant window size
-    features_names = paste(as.data.frame(chr_ranges)$seqnames,
-                           as.data.frame(chr_ranges)$start,
-                           as.data.frame(chr_ranges)$end, sep="_")
-  }
-  if(featureType[1] == "peak") {
-    size_peaks = c(1000,2500,7999,10000,150000,10^6) #Different size of peaks
-    peaks = sapply(split( 1:features , sample(length(size_peaks), features , repl = TRUE) ), length)
-    chr_ranges_list = GRangesList()
-    for(i in 1:length(peaks)){
-      chr_ranges = unlist(tileGenome(setNames(width(chr),seqnames(chr)),
-                                     tilewidth = size_peaks[i], cut.last.tile.in.chrom = FALSE))
-      chr_ranges_list[[i]] = chr_ranges[sample(1:length(chr_ranges),size = peaks[i]),]
-    }
-    chr_ranges = GenomicRanges::sort.GenomicRanges(unlist(chr_ranges_list))[1:features]
-    
-    features_names = paste(as.data.frame(chr_ranges)$seqnames,
-                           as.data.frame(chr_ranges)$start,
-                           as.data.frame(chr_ranges)$end, sep="_")
-  }
-  if(featureType[1] == "gene") {
-    eval(parse(text = paste0("chr <- ChromSCape::",ref,".GeneTSS"))) #load species chromsizes
-    features_names = as.character(sample(chr$gene,features,replace = FALSE))
-  }
-  vec = rpois(cells*features,0.5) #Add count to values > 0, iteratively
-  for(i in 1:10) vec[vec >= i] = vec[vec >= i]  +  i^2*rpois(length(vec[vec >= i]),0.5)
-  mat = matrix(vec, nrow = features, ncol = cells, 
-               dimnames = list( features_names,cell_names))
-  annot = data.frame(cell_id = cell_names,
-                     sample_id = sample,
-                     batch_id = batches,
-                     total_counts = Matrix::colSums(mat))
-  if(sparse) return(list("mat" =  as(mat,"dgCMatrix"), "annot" = annot)) else return(list("mat" =  mat, "annot" = annot))
-}
-
 out = create_scDataset_raw(featureType = "window")
 mat = out$mat
 annot = out$annot
@@ -85,7 +20,7 @@ test_that("Correlation & hierarchical clustering - Wrong inputs.", {
   expect_error(correlation_and_hierarchical_clust_scExp(scExp, hc_linkage = "ABC"))
   
   scExp. = scExp
-  reducedDim(scExp., "PCA") = NULL  
+  SingleCellExperiment::reducedDim(scExp., "PCA") = NULL  
   expect_error(correlation_and_hierarchical_clust_scExp(scExp.))
   
 })
@@ -100,9 +35,9 @@ test_that("Correlation & hierarchical clustering - Right inputs.", {
 
   expect_is(scExp@metadata$hc_cor,
                   "hclust")
-  expect_type(reducedDim(scExp,"Cor"),
+  expect_type(SingleCellExperiment::reducedDim(scExp,"Cor"),
                   "double")
-  expect_is(reducedDim(scExp,"Cor"),
+  expect_is(SingleCellExperiment::reducedDim(scExp,"Cor"),
                   "matrix")
 })
 
@@ -117,7 +52,7 @@ test_that("Filtering lowly correlated cells - Wrong inputs.", {
   
   
   scExp. = scExp
-  reducedDim(scExp., "Cor") = NULL  
+  SingleCellExperiment::reducedDim(scExp., "Cor") = NULL  
   expect_error(filter_correlated_cell_scExp(scExp.))
   
 })
@@ -142,32 +77,21 @@ test_that("Consensus Clustering - Wrong inputs.", {
   expect_error(consensus_clustering_scExp(NULL))
 
   expect_error(consensus_clustering_scExp(scExp_cf,maxK = NULL))
-
-  scExp_cf. = scExp_cf
-  reducedDim(scExp_cf., "Cor") = NULL  
-  expect_error(consensus_clustering_scExp(scExp_cf.))
-  
 })
 
-scExp_cf = consensus_clustering_scExp(scExp_cf, prefix = "tests/test_scChIP/consensus_test",plot_consclust = "pdf")
+scExp_cf = consensus_clustering_scExp(scExp_cf)
 
 test_that("Consensus Clustering - Right inputs.", {
   expect_s4_class(scExp_cf,
                   "SingleCellExperiment")
 
-  expect_equal(file.exists("tests/test_scChIP/consensus_test/consensus.pdf"),TRUE)
-  expect_equal(file.exists("tests/test_scChIP/consensus_test/icl001.png"),TRUE)
-  if(dir.exists("tests/test_scChIP/consensus_test")) unlink("tests/test_scChIP/consensus_test",recursive = TRUE)
-  
   # No filtering :
   expect_is(scExp_cf@metadata$consclust, "list")
   expect_is(scExp_cf@metadata$consclust[[1]], "matrix")
   expect_is(scExp_cf@metadata$consclust[[2]], "list")
-  expect_is(scExp_cf@metadata$consclust[[2]]$consensusMatrix, "matrix")
-  expect_is(scExp_cf@metadata$consclust[[2]]$ml, "matrix")
   expect_is(scExp_cf@metadata$icl, "list")
   expect_is(scExp_cf@metadata$icl$clusterConsensus, "matrix")
-  expect_is(scExp_cf@metadata$icl$itemConsensus, "matrix")
+  expect_is(scExp_cf@metadata$icl$itemConsensus, "data.frame")
   
 })
 
@@ -191,10 +115,9 @@ test_that("Choosing clusters - Right inputs.", {
                   "SingleCellExperiment")
   
   # No filtering :
-  expect_is(colData(scExp_cf)$chromatin_group, "character")
-  expect_is(colData(scExp_cf)$chromatin_group_color, "character")
-  expect_is(reducedDim(scExp_cf,"ConsensusAssociation"), "matrix")
-  expect_is(scExp_cf@metadata$hc_consensus_association, "hclust")
+  expect_is(SingleCellExperiment::colData(scExp_cf)$cell_cluster, "character")
+  expect_is(SingleCellExperiment::colData(scExp_cf)$cell_cluster_color, "character")
+  expect_is(scExp_cf@metadata$hc_cor, "hclust")
 })
 
 
