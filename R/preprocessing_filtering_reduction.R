283d8178e5204d4854f1151461cc36ced7aa3d00
Pacome <pacome.pr@gmail.com>
2021-03-22 17:32:42 +0100

    Removed 'detected' from colData for mem consumption reasons

--- R/preprocessing_filtering_reduction.R
+++ R/preprocessing_filtering_reduction.R
@@ -1227,14 +1227,14 @@ filter_scExp =  function (
     scExp <- scExp[fixedFeature,]
     SummarizedExperiment::colData(scExp)$total_counts = 
         colSums(SingleCellExperiment::counts(scExp))
-    if(!is.na(ncol(scExp) * nrow(scExp))){
-        SummarizedExperiment::colData(scExp)$detected = 
-            apply(SingleCellExperiment::counts(scExp), 2,
-                  function(i) length(which(i>0)))
-    } else{
-        SummarizedExperiment::colData(scExp)$detected = 
-            SummarizedExperiment::colData(scExp)$total_counts
-    }
+    # if(!is.na(ncol(scExp) * nrow(scExp))){
+    #     SummarizedExperiment::colData(scExp)$detected = 
+    #         apply(SingleCellExperiment::counts(scExp), 2,
+    #               function(i) length(which(i>0)))
+    # } else{
+    #     SummarizedExperiment::colData(scExp)$detected = 
+    #         SummarizedExperiment::colData(scExp)$total_counts
+    # }
     return(scExp)
 }
 
