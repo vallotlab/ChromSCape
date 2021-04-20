362461e2bb8de9db2e80912375b9b89d9e602d3c
Pacome Prompsy <pacome.pr@gmail.com>
2021-04-20 15:50:31 +0200

    CNA study

--- R/copy_number.R
+++ R/copy_number.R
@@ -1,4 +1,28 @@
 
+#' @title Map features onto cytobands  
+#'   
+#' @description Map the features of a SingleCellExperiment onto the cytobands 
+#' of a given genome. Some features might not be mapped to any cytobands (e.g.
+#'  if they are not in the canconical chromosomes), and are removed from the
+#'  returned object.
+#'
+#' @details 
+#' The cytobands are an arbitrary cutting of the genome that dates back to 
+#' staining metaphase chromosomes with Giemsa.
+#' 
+#' @param scExp A SingleCellExperiment with genomic coordinate as features 
+#' (peaks or bins) 
+#' @param ref_genome Reference genome ('hg38' or 'mm10') 
+#'
+#' @return A data.frame of the SCE features with their corresponding cytoband
+#' name
+#' 
+#' @importFrom SummarizedExperiment rowRanges seqnames
+#' @importFrom S4Vectors queryHits
+#' @importFrom GenomicRanges findOverlaps
+#' @export
+#' @examples matching_cyto = get_cyto_features(scExp, ref_genome="hg38")
+#'  
 get_cyto_features <- function(scExp, ref_genome = c("hg38", "mm10")[1] ){
     
     canonical_chr <- eval(parse(text = paste0("ChromSCape::",
@@ -12,22 +36,48 @@ get_cyto_features <- function(scExp, ref_genome = c("hg38", "mm10")[1] ){
     cyto = as(cyto,"GRanges")
     
     # Get genomic bins
-    bins = rowRanges(scExp)
-    bins = bins[which(as.character(seqnames(bins)) %in% as.character(seqnames(canonical_chr)))]
+    bins = SummarizedExperiment::rowRanges(scExp)
+    bins = bins[which(as.character(SummarizedExperiment::seqnames(bins)) %in%
+                          as.character(SummarizedExperiment::seqnames(canonical_chr)))]
     
     matching_cyto = data.frame(ID = bins$ID, hits = "")
     
     # Get which bins goes into which cyto band
     for(i in unique(cyto$cytoBand)){
         cyto. = cyto[which(cyto$cytoBand == i)]
-        matching_cyto$hits[queryHits(findOverlaps(bins,cyto.))] = i
+        matching_cyto$hits[S4Vectors::queryHits(GenomicRanges::findOverlaps(bins,cyto.))] = i
     }
     
     matching_cyto = matching_cyto[which(matching_cyto$hits!=""),]
     return(matching_cyto)
 }
 
-calculate_cyto_mat <- function(scExp, ref_genome = c("hg38","mm10")){
+#' @title  Calculate Fraction of reads in each cytobands
+#' 
+#' @description
+#' Re-Count binned reads onto cytobands and calculate the fraction of reads in 
+#' each of the cytoband in each cell. For each cell, the fraction of reads in
+#'  any given cytoband is calculated. 
+#' Cytobands are considered large enough in order that a variation at the 
+#' cytoband level is not considered as an epigenetic event but as a genetic 
+#' event, e.g. Copy Number Alterations.
+#' 
+#' @param scExp A SingleCellExperiment with genomic coordinate as features 
+#' (peaks or bins) 
+#' @param ref_genome Reference genome ('hg38' or 'mm10') 
+#'
+#' @return The SCE with the fraction of reads in each cytobands in each cells
+#' (of dimension cell x cytoband ) in the  reducedDim slot "cytoBand".
+#' 
+#' @importFrom dplyr group_by summarise everything
+#' @importFrom Matrix colSums
+#' @importFrom SingleCellExperiment reducedDim
+#' @export
+#' @examples 
+#' scExp = calculate_cyto_mat(scExp, ref_genome="hg38")
+#' reducedDim(scExp, "cytoBand") 
+#' 
+calculate_cyto_mat <- function(scExp, ref_genome = c("hg38","mm10")[1]){
     matching_cyto = get_cyto_features(scExp,ref_genome)
     mat = counts(scExp)[match(matching_cyto$ID, rownames(scExp)),]
     gc()
@@ -41,35 +91,92 @@ calculate_cyto_mat <- function(scExp, ref_genome = c("hg38","mm10")){
     # Normalize by library size
     cyto_mat. = cyto_mat[,-1]
     rownames(cyto_mat) = cyto_mat$`matching_cyto$hits`
-    cyto_mat[,-1] = 100* sweep(cyto_mat.,2,colSums(cyto_mat.),`/`)
+    cyto_mat[,-1] = 100* base::sweep(cyto_mat.,2,Matrix::colSums(cyto_mat.),`/`)
     cyto_mat = cyto_mat[,-1]
     
     SingleCellExperiment::reducedDim(scExp, "cytoBand") = t(cyto_mat)
     return(scExp)
 }
 
+
+#' @title Retrieve the cytobands with the most variable fraction of reads
+#' 
+#' @description
+#' Given a SingleCellExperiment object with the slot "cytoBand" containing the
+#' fraction of reads in each cytoband, calculates the variance of each cytoband
+#' and returns a data.frame with the top variables cytobands. Most cytobands are
+#' expected to be unchanged between normal and tumor samples, therefore focusing
+#' on the top variable cytobands enable to focus on the most interseting 
+#' regions. 
+#' 
+#' @param scExp A SingleCellExperiment with "cytoBand" reducedDim slot filled.
+#' @param top Number of cytobands to return (50).
+#'
+#' @return A data.frame of the top variable cytoBands and their variance
+#' 
+#' @importFrom dplyr group_by summarise desc arrange
+#' @importFrom tidyr gather
+#' @importFrom SingleCellExperiment reducedDim
+#' @export
+#' @examples 
+#' 
+#' scExp = calculate_cyto_mat(scExp, ref_genome="hg38")
+#' get_most_variable_cyto(scExp, top=50)
+#'  
 get_most_variable_cyto <- function(scExp, top = 50){
     # Retrieve top variable cytobands
-    cyto_df = as.data.frame(t(SingleCellExperiment::reducedDim(scExp, "cytoBand")))
+    cyto_df = as.data.frame(
+        t(SingleCellExperiment::reducedDim(scExp, "cytoBand")))
     cyto_df$cytoBand = rownames(cyto_df)
     
     cyto_df = cyto_df %>% tidyr::gather("cell_id","Fri_cyto",-ncol(cyto_df))
-    top_var = cyto_df %>% dplyr::group_by(cytoBand) %>% dplyr::summarise(var = var(Fri_cyto)) %>% 
+    top_var = cyto_df %>% dplyr::group_by(cytoBand) %>%
+        dplyr::summarise(var = var(Fri_cyto)) %>% 
         dplyr::arrange(dplyr::desc(var)) %>% head(n = top)
     
     return(top_var)
 }
 
+
+#' @title Calculate the log2-ratio of tumor vs normal fraction of reads in 
+#' cytobands 
+#' 
+#' @description
+#' Given a SingleCellExperiment object with the slot "cytoBand" containing the
+#' fraction of reads in each cytoband, calculates the log2-ratio of tumor vs
+#' normal fraction of reads in cytobands, cell by cell.  
+#' If the average signal in normal sample in a cytoband is 0, set this value to
+#' 1 so that the ratio won't affect the fraction of read value.
+#' 
+#' @param scExp A SingleCellExperiment with "cytoBand" reducedDim slot filled.
+#' - see  \code{\link{calculate_cyto_mat}}
+#' @param controls Sample IDs or Cell IDs of the normal sample to take as 
+#' reference.
+#'
+#' @return The SCE with the log2-ratio of fraction of reads in each cytobands 
+#' in each cells (of dimension cell x cytoband ) in the  reducedDim 
+#' slot "logRatio_cytoBand".
+#' 
+#' @importFrom Matrix colMeans
+#' @importFrom SingleCellExperiment reducedDim
+#' @export
+#' @examples 
+#' 
+#' scExp = calculate_cyto_mat(scExp, ref_genome="hg38")
+#' scExp = calculate_logRatio_CNA(scExp, controls=unique(scExp$sample_id)[1])
+#' reducedDim(scExp, "logRatio_cytoBand")
+#' 
 calculate_logRatio_CNA <- function(scExp, controls){
     cyto_mat = SingleCellExperiment::reducedDim(scExp, "cytoBand")
     
     is_cell_id = ifelse(all(controls %in% scExp$cell_id), TRUE, FALSE)
     if(is_cell_id){
-        normal_signal_per_chr = setNames(colMeans(cyto_mat[controls,]),
+        normal_signal_per_chr = setNames(Matrix::colMeans(cyto_mat[controls,]),
                                          colnames(cyto_mat[controls,]))
     } else{
-        controls_cell = scExp$cell_id[which(scExp$sample_id %in% controls)]
-        normal_signal_per_chr = setNames(colMeans(cyto_mat[controls_cell,]),
+        controls_cell = as.character(
+            scExp$cell_id[which(scExp$sample_id %in% controls)])
+        normal_signal_per_chr = setNames(Matrix::colMeans(cyto_mat[controls_cell,]),
                                          colnames(cyto_mat[controls_cell,]))
     }
     
@@ -90,12 +197,44 @@ calculate_logRatio_CNA <- function(scExp, controls){
     return(scExp)
 } 
 
-calculate_gain_or_loss <- function(scExp, controls, quantiles = c(0.01, 0.99)){
+#' @title Estimate the copy gains/loss of tumor vs normal based on log2-ratio of
+#' fraction of reads
+#' 
+#' @description
+#' Given a SingleCellExperiment object with the slot "logRatio_cytoBand" containing the
+#' log2-ratio of the fraction of reads in each cytoband, estimate if the 
+#' cytoband was lost or acquired a gain in a non-quantitative way. To do so, 
+#' the quantiles distribution of the normal cells are calculated, and any 
+#' cytoband below or above will be considered as a loss/gain. The False 
+#' Discovery Rate is directly proportional to the quantiles.  
+#' 
+#' @param scExp A SingleCellExperiment with "logRatio_cytoBand" reducedDim slot 
+#' filled. See  \code{\link{calculate_logRatio_CNA}}
+#' @param controls Sample IDs or Cell IDs of the normal sample to take as 
+#' reference.
+#' @param quantiles Quantiles of normal log2-ratio distribution below/above 
+#' which cytoband is considered to be a loss/gain. (c(0.05,0.95))
+#'
+#' @return The SCE with the gain or loss in each cytobands 
+#' in each cells (of dimension cell x cytoband ) in the  reducedDim 
+#' slot "gainOrLoss_cytoBand".
+#' 
+#' @export
+#' @examples 
+#' 
+#' scExp = calculate_cyto_mat(scExp, ref_genome="hg38")
+#' scExp = calculate_logRatio_CNA(scExp, controls=unique(scExp$sample_id)[1])
+#' scExp = calculate_gain_or_loss(scExp, controls=unique(scExp$sample_id)[1])
+#' reducedDim(scExp, "gainOrLoss_cytoBand")
+#' 
+calculate_gain_or_loss <- function(scExp, controls, quantiles = c(0.05, 0.95)){
     logRatio_mat = SingleCellExperiment::reducedDim(scExp, "logRatio_cytoBand")
     
     is_cell_id = ifelse(all(controls %in% scExp$cell_id), TRUE, FALSE)
     if(!is_cell_id){
-        controls = scExp$cell_id[which(scExp$sample_id %in% controls)]
+        controls = as.character(
+            scExp$cell_id[which(scExp$sample_id %in% controls)]
+        )
     }
     
     gain_or_loss_cytoBand = logRatio_mat
@@ -115,14 +254,70 @@ calculate_gain_or_loss <- function(scExp, controls, quantiles = c(0.01, 0.99)){
 }
 
 
+#' @title Estimate copy number alterations in cytobands
+#' 
+#' @description
+#' Cytobands are considered large enough in order that a variation at the 
+#' cytoband level is not considered as an epigenetic event but as a genetic 
+#' event, e.g. Copy Number Alterations. The function successively : 
+#' - Calculates the fraction of reads in each cytoband (FrCyto). See \code{\link{calculate_cyto_mat}}
+#' - Calculates the log2-ratio FrCyto of each cell by the average FrCyto in normal cells. See  \code{\link{calculate_logRatio_CNA}}
+#' - Estimates if there was a gain or a loss of copy in each cyto band. See  \code{\link{calculate_gain_or_loss}}
+#'   
+#'  The corresponding matrices are accessibles in the reducedDim slots
+#'   "cytoBands", "logRatio_cytoBands" and "gainOrLoss_cytoBands" respectively.
+#'
+#' @param scExp A SingleCellExperiment with "logRatio_cytoBand" reducedDim slot 
+#' filled. See  \code{\link{calculate_logRatio_CNA}}
+#' @param control_samples Sample IDs of the normal sample to take as 
+#' reference.
+#' @param quantiles_to_define_gol Quantiles of normal log2-ratio distribution
+#'  below/above which cytoband is considered to be a loss/gain. (c(0.05,0.95)). 
+#' See \code{\link{calculate_gain_or_loss}}
+#'
+#' @return The SCE with the fraction of reads, log2-ratio and gain or loss
+#'  in each cytobands in each cells (of dimension cell x cytoband) 
+#'  in the  reducedDim slots.
+#' 
+#' @export
+#' @examples 
+#' 
+#' scExp = calculate_CNA(scExp,  control_samples = unique(scExp$sample_id)[1],
+#' ref_genome="hg38", quantiles_to_define_gol = c(0.05,0.95))
+#' reducedDim(scExp, "cytoBand")
+#' reducedDim(scExp, "logRatio_cytoBand")
+#' reducedDim(scExp, "gainOrLoss_cytoBand")
+#'
 calculate_CNA <- function(scExp, control_samples = unique(scExp$sample_id)[1],
-                          ref_genome = c("hg38","mm10")[1]){
+                          ref_genome = c("hg38","mm10")[1],
+                          quantiles_to_define_gol = c(0.05,0.95)
+                          ){
     scExp = calculate_cyto_mat(scExp, ref_genome = ref_genome)
     scExp = calculate_logRatio_CNA(scExp, controls = control_samples)
-    scExp = calculate_gain_or_loss(scExp, controls = control_samples)
+    scExp = calculate_gain_or_loss(scExp, controls = control_samples,
+                                   quantiles =  quantiles_to_define_gol)
     return(scExp)
 }
 
+#' @title Plot Gain or Loss of cytobands of the most variables cytobands
+#' 
+#' 
+#' @param scExp A SingleCellExperiment with "gainOrLoss_cytoBand" reducedDim slot 
+#' filled. See  \code{\link{calculate_gain_or_loss}}
+#' @param cells Cell IDs of the tumor samples to 
+#' @param top Number of most variables cytobands to plot
+#'
+#' @return Plot the gains/lost in the selected cells of interest as multiple
+#' barplots
+#' 
+#' @export
+#' @examples 
+#' 
+#' scExp = calculate_CNA(scExp,  control_samples = unique(scExp$sample_id)[1],
+#' ref_genome="hg38", quantiles_to_define_gol = c(0.05,0.95))
+#' plot_gain_or_loss_barplots(scExp, cells = scExp$cell_id[which(
+#' scExp$sample_id %in% unique(scExp$sample_id)[2])])
+#'
 plot_gain_or_loss_barplots <- function(scExp, cells = NULL, top = 20){
     gain_or_loss_cytoBand = reducedDim(scExp, "gainOrLoss_cytoBand")
     if(!is.null(cells)){
@@ -146,6 +341,26 @@ plot_gain_or_loss_barplots <- function(scExp, cells = NULL, top = 20){
 }
 
 
+
+#' @title Plot Gain or Loss of cytobands of the most variables cytobands 
+#' 
+#' 
+#' @param scExp A SingleCellExperiment with "logRatio_cytoBand" reducedDim slot 
+#' filled. See  \code{\link{calculate_logRatio_CNA}}
+#' @param cells Cell IDs of the tumor samples to 
+#' @param top Number of most variables cytobands to plot
+#'
+#' @return Plot the gains/lost in the selected cells of interest as multiple
+#' barplots
+#' 
+#' @export
+#' @examples 
+#' 
+#' scExp = calculate_CNA(scExp,  control_samples = unique(scExp$sample_id)[1],
+#' ref_genome="hg38", quantiles_to_define_gol = c(0.05,0.95))
+#' plot_gain_or_loss_barplots(scExp, cells = scExp$cell_id[which(
+#' scExp$sample_id %in% unique(scExp$sample_id)[2])])
+#'
 plot_gain_or_loss_barplots <- function(scExp, cells = NULL, top = 20){
     gain_or_loss_cytoBand = reducedDim(scExp, "gainOrLoss_cytoBand")
     if(!is.null(cells)){
@@ -169,6 +384,22 @@ plot_gain_or_loss_barplots <- function(scExp, cells = NULL, top = 20){
 }
 
 
+#' @title Plot UMAP colored by Gain or Loss of cytobands
+#' 
+#' 
+#' @param scExp A SingleCellExperiment with "gainOrLoss_cytoBand" reducedDim 
+#' slot filled. See  \code{\link{calculate_gain_or_loss}}
+#' @param cytoBand Which cytoBand to color cells by
+#'
+#' @return Plot the gains/lost of the cytoband overlayed on the epigenetic UMAP.
+#' 
+#' @export
+#' @examples 
+#' 
+#' scExp = calculate_CNA(scExp,  control_samples = unique(scExp$sample_id)[1],
+#' ref_genome="hg38", quantiles_to_define_gol = c(0.05,0.95))
+#' plot_reduced_dim_scExp_CNA(scExp, get_most_variable_cyto(scExp)$cytoBand[1])
+#'
 plot_reduced_dim_scExp_CNA <- function(scExp, cytoBand){
     gain_or_loss_cytoBand = reducedDim(scExp, "gainOrLoss_cytoBand")
     SummarizedExperiment::colData(scExp)[,paste0("Gain_or_Loss_",cytoBand)] = 
